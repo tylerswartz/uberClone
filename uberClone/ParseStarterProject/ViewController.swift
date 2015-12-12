121795e09d0c2884f5876d43a57ee075f42bd675
Tyler Swartz <tgswartz@gmail.com>
2015-12-12 13:26:01 -0800

    added storyboard outlets for login. added alert function. added functions to tap or press return to dismiss the keyboard

--- uberClone/ParseStarterProject/ViewController.swift
+++ uberClone/ParseStarterProject/ViewController.swift
@@ -7,8 +7,39 @@
 import UIKit
 import Parse
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    func displayAlert(title:String,message:String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var `switch`: UISwitch!
+    @IBOutlet weak var driverLabel: UILabel!
+    @IBOutlet weak var riderLabel: UILabel!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBAction func signUp(sender: AnyObject) {
+    
+        if username.text == "" || password.text == "" {
+            
+            displayAlert("Missing field(s)", message:"Username and password are required.")
+            
+        }
+    
+    }
+    
+    @IBOutlet weak var toggleSignUpButton: UIButton!
+    @IBAction func toggleSignUp(sender: AnyObject) {
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do an y additional setup after loading the view, typically from a nib.
@@ -19,11 +50,33 @@ class ViewController: UIViewController {
             print("Object has been saved.")
         }
         
+        //looks for single or multiple taps to dismiss keyboard
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+        
+        //look for return button to dismiss keyboard
+        self.username.delegate = self
+        self.password.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    //hide keyboard when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    //hide keyboard on return button
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
 }
 
